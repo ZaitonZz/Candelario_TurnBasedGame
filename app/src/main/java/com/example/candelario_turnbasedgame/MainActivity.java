0fafbc9280822e3ad14be26341da2c26c5603f09
BuildTools <unconfigured@null.spigotmc.org>
2022-02-07 04:56:57 +0800

    uploading this to github in order to not accidentally mess with the code in my free time

---
+++ app/src/main/java/com/example/candelario_turnbasedgame/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.candelario_turnbasedgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_main);
+    }
+}
